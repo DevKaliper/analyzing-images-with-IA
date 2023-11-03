46174847095d40cd722f85b27c4267f42a1a893b
Dev Kaliper <gxlthz@gmail.com>
2023-11-03 14:36:00 -0500

    Refactor computer vision UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState('');
 
-  return (
+  const handleChangeValue = (e) => {
+    setUrl(e.target.value)
+  }
+
+  const handleAnalyze = () => {
+    console.log("Analyze")
+  }
+
+  const handleGenerate = () => {
+    console.log("Generate")
+  }
+  
+  return(
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Computer vision</h1>
+    <hr />
+
+    <h3>Insert URL or type promt:</h3>
+    <input type="text" placeholder='Enter url to analyze or textual prompt to generate an image' onChange={(e)=>handleChangeValue(e)} value={url}/>
+    <button onClick={handleAnalyze}>Analyze</button>
+    <button onClick={handleGenerate}>Generate</button>
+
+    
     </>
   )
 }
