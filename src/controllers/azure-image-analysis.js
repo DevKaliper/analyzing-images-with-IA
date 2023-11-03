c8a5efb5368ca915cff9fd18e5571abd71747073
Dev Kaliper <gxlthz@gmail.com>
2023-11-03 14:44:11 -0500

    Refactor code to improve performance and readability.

---
+++ src/controllers/azure-image-analysis.js
@@ -0,0 +1,20 @@
+// Importa la biblioteca dotenv
+
+const analyzeImage = async (img) => {
+    const url = `${import.meta.env.VITE_POINT}/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=tags,read,caption,denseCaptions,smartCrops,objects,people`;
+  
+    const data = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Ocp-Apim-Subscription-Key": import.meta.env.KEY,
+      },
+  
+      body: JSON.stringify({ url: img }), // body data type must match "Content-Type" header
+    });
+  
+    const response = await data.json();
+    console.log(response);
+  };
+  export default analyzeImage;
+  
