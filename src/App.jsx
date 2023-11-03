42301e13d57265df66bef7d0afceae23ab7c4fb4
Dev Kaliper <gxlthz@gmail.com>
2023-11-03 15:30:59 -0500

    Add result display and clear button to UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,44 @@
 import { useState } from 'react'
 import analyzeImage from './controllers/azure-image-analysis';
+
+const DisplayResults =  ({result}) => {
+  const datos = [JSON.stringify(result)]
+  return(
+    <div>
+      {datos.map((dato, index) => (
+        <p key={index}>{dato}</p>
+      ))}
+    </div>
+  )
+  
+}
 function App() {
   const [url, setUrl] = useState('');
+  const[loading, setLoading] = useState(false);
+  const [result, setResult] = useState(null);
 
   const handleChangeValue = (e) => {
     setUrl(e.target.value)
   }
 
   const handleAnalyze = async() => {
+    setLoading(true)
     const result = await analyzeImage(url);
-    console.log(result)
+    setLoading(false)
+    setResult(result)
+    
   }
+
+
 
   const handleGenerate = () => {
     console.log("Generate")
+  }
+
+  const handleClear = () => {
+    setLoading(false)
+    setResult(null)
+    setUrl('')
   }
   
   return(
@@ -25,6 +50,14 @@ function App() {
     <input type="text" placeholder='Enter url to analyze or textual prompt to generate an image' onChange={(e)=>handleChangeValue(e)} value={url}/>
     <button onClick={handleAnalyze}>Analyze</button>
     <button onClick={handleGenerate}>Generate</button>
+    <button onClick={handleClear}>clear</button>
+
+    <br />
+
+    {loading && <p>Loading...</p>}
+    {!loading && result &&  
+    <DisplayResults result={result} />}
+
 
     
     </>
